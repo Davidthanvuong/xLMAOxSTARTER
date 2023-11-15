1aa8f4df557a7a0f3ca4e2f7572e76a7005d0270
khang200923 <92591231+khang200923@users.noreply.github.com>
2023-11-15 21:47:20 +0700

    Add score mechanics

---
+++ DATA/ScorePerk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class ScorePerk
+{
+    public string AppName { get; set; }
+    public int ScoreCost { get; set; }
+    public float RewardTime { get; set; }
+
+    public ScorePerk(string appName, int scoreCost, float rewardTime) {
+        AppName = appName; 
+        ScoreCost = scoreCost; 
+        RewardTime = rewardTime;
+    }
+
+    public static List<ScorePerk> SamplePerks = new()
+    {
+        new("Weird App", 100, 1800),
+        new("Weird App", 150, 3600), //Wait for longer to gain more reward
+    };
+}
