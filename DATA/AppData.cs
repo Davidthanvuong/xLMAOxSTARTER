d947a82f36402e80364b30d9d250277328e63d35
khang200923 <92591231+khang200923@users.noreply.github.com>
2023-11-17 16:59:17 +0700

    Add AppData for sharing data (I don't know if I implemented it correctly)

---
+++ DATA/AppData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Storage;
+using Newtonsoft.Json;
+
+namespace xLMAOxSTARTER.DATA
+{
+    public class AppData
+    {
+        public async Task Set(string key, object value) 
+        {
+            string json = JsonConvert.SerializeObject(value);
+            await SecureStorage.Default.SetAsync("appdata_" + key, json);
+        }
+        public async Task<object> Get(string key)
+        {
+            string value = await SecureStorage.Default.GetAsync("appdata_" + key);
+            return JsonConvert.DeserializeObject<object>(value);
+        }
+        public bool Remove(string key)
+        {
+            return SecureStorage.Default.Remove("appdata_" + key);
+        }
+    }
+}
