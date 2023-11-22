a3479bbd845374f7f38f1a6009880eee87416e33
khang200923 <92591231+khang200923@users.noreply.github.com>
2023-11-22 12:05:53 +0700

    Fix and add appInfos var

--- DATA/AppData.cs
+++ DATA/AppData.cs
@@ -10,19 +10,24 @@ namespace xLMAOxSTARTER.DATA
 {
     public class AppData
     {
-        public async Task Set(string key, object value) 
+        public async static Task Set<T>(string key, T value) 
         {
             string json = JsonConvert.SerializeObject(value);
             await SecureStorage.Default.SetAsync("appdata_" + key, json);
         }
-        public async Task<object> Get(string key)
+        public async static Task<T> Get<T>(string key)
         {
             string value = await SecureStorage.Default.GetAsync("appdata_" + key);
-            return JsonConvert.DeserializeObject<object>(value);
+            return JsonConvert.DeserializeObject<T>(value);
         }
         public bool Remove(string key)
         {
             return SecureStorage.Default.Remove("appdata_" + key);
+        }
+        public List<AppInfo> appInfos
+        {
+            get => Get<List<AppInfo>>("appinfos").GetAwaiter().GetResult();
+            set { Set("appinfos", value).Wait(); }
         }
     }
 }
