ee10f7d016c116a7da4dde0106a4986aad19e7b2
khang200923 <92591231+khang200923@users.noreply.github.com>
2023-11-24 13:25:56 +0700

    Refactor milestones

---
+++ DATA/Extension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xLMAOxSTARTER.DATA
+{
+    public static class Extension
+    {
+        public static int FloorMult(this float val, int mult, int add = 1) => ((int)val + add) * mult;
+    }
+}
--- DATA/Milestone.cs
+++ DATA/Milestone.cs
@@ -29,32 +29,32 @@ public class Milestone
     {
         new Milestone(
             0, "XP Wizard",
-            () => "Have " + (Math.Floor(AppData.Milestones[0].ScoreFunc()) + 1) * 1000 + " XP in total",
+            () => $"Have {AppData.Milestones[0].ScoreFunc().FloorMult(1000)} XP in total",
             () => AppData.XPdata.totalXP / 1000
             ),
         new Milestone(
             1, "Healthy Consumer",
-            () => "Have " + (Math.Floor(AppData.Milestones[1].ScoreFunc()) + 1) * 200 + " XP gained from good apps in total",
+            () => $"Have {AppData.Milestones[1].ScoreFunc().FloorMult(200)} XP gained from good apps in total",
             () => AppData.XPdata.healthyTotalXP / 200
             ),
         new Milestone(
             2, "Successful Achiever",
-            () => "Have " + (Math.Floor(AppData.Milestones[2].ScoreFunc()) + 1) * 200 + " XP gained from milestones in total",
+            () => $"Have {AppData.Milestones[2].ScoreFunc().FloorMult(200)} XP gained from milestones in total",
             () => AppData.XPdata.milestoneTotalXP / 200
             ),
         new Milestone(
             3, "Energetic Wealth",
-            () => "Have " + (Math.Floor(AppData.Milestones[3].ScoreFunc()) + 1) * 500 + " acquired XP at the same time",
+            () => $"Have {AppData.Milestones[3].ScoreFunc().FloorMult(500)} acquired XP at the same time",
             () => AppData.XPdata.highestXP / 500
             ),
         new Milestone(
             4, "Productivity Paragon",
-            () => "Avoid using bad apps for consecutive " + (Math.Floor(AppData.Milestones[4].ScoreFunc()) + 5) * 4 + " hours",
+            () => $"Avoid using bad apps for consecutive {AppData.Milestones[4].ScoreFunc().FloorMult(4, 5)} hours",
             () => Math.Min(0, (AppData.MiscGame.highestNoBadAppTime / 3600) * 4 - 5)
             ),
         new Milestone(
             5, "Tempo Master",
-            () => "Be offline for consecutive " + (Math.Floor(AppData.Milestones[5].ScoreFunc()) + 4) * 5 + " hours",
+            () => $"Be offline for consecutive {AppData.Milestones[5].ScoreFunc().FloorMult(5, 4)} hours",
             () => Math.Min(0, (AppData.MiscGame.highestOfflineTime / 3600) * 5 - 4)
             ),
     };
